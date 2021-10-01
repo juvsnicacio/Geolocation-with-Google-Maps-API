e37249872b2930c0f5b871fefda951150ae73071
Juvanderson Nicacio <juvandersonns@gmail.com>
2021-10-01 15:28:21 -0300

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -13,11 +13,9 @@ function pegaLocal($lat,$long){
     // $full_address=$json_data->results[0]->formatted_address;
     // echo "<br>".$full_address;
     if($full_address=$json_data->results[0]->formatted_address){
-        // echo "<br><br>".$full_address;
         return $full_address;
     }
     else{
-        // pegaLocal($lat, $long);
         return "A requisição não obteve resposta do <b>http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&sensor=true</b>";
     }
 }
